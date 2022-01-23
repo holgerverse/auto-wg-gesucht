96071b7fe13defc3c3f62164f756b059ca4e3a8b
hziemons <hannah.ziemons@taod.de>
2022-01-23 21:11:50 +0100

    Fix bugs + refactoring

--- src/main.py
+++ src/main.py
@@ -116,7 +116,7 @@ def go_to_filter_page(driver):
             filter_page_reached = True
             print("Reached filter page.")
         except IndexError:
-            login_button = driver.find_elements((By.LINK_TEXT, "Login"))
+            login_button = driver.find_elements((By.TAG_NAME, "button"))
             print("Type login button:" + str(type(login_button)))
             print(login_button)
             login_button[0].click()
@@ -124,26 +124,27 @@ def go_to_filter_page(driver):
 
 
 def get_flat_ad_info_as_dict(driver):
+    time.sleep(15)
     ad_boxes_list = WebDriverWait(driver, 20).until(
         exp_cond.visibility_of_all_elements_located((By.CLASS_NAME, 'card_body')))
     print("Number of ads on page:\t" + str(len(ad_boxes_list)))
     ad_dict = {}
     for key, value in enumerate(ad_boxes_list):
+        sub_title = value.find_elements(By.CLASS_NAME, "col-xs-11") #  click_button_by_class_name(driver, "col-xs-11", None, False).text
         title = value.find_elements(By.TAG_NAME, "h3")
-        sub_title = click_button_by_class_name(driver, "col-xs-11", None, False).text
-        people_count = int(sub_title.split('er WG')[0]) - 1
-        street = sub_title.split('| ')[2]
         link = title[0].find_elements(By.LINK_TEXT, title[0].text)[0].get_attribute('href')
         online_list = value.find_elements(By.CLASS_NAME, "flex_space_between")
-        for sub_key, sub_value in enumerate(online_list):
-            if "Online:" in online_list[sub_key].text:
-                online = online_list[sub_key].text
-                contact = online.split('Online:')[0].replace("\n", "")
-                online = online.split('Online:')[1]
+        oll_text_values = [el.text for el in online_list]
+        for el_text in oll_text_values:
+            if "Online:" in el_text:
+                people_count = int(sub_title[0].text.split('er WG')[0]) - 1
+                street = sub_title[0].text.split('| ')[2]
+                contact = el_text.split('Online:')[0].replace("\n", "")
+                online_since = el_text.split('Online:')[1]
                 ad_dict[key] = {
                     'title': title[0].text,
                     'link': link,
-                    'online': online,
+                    'online': online_since,
                     'contact': contact,
                     'people_count': people_count,
                     'street': street
@@ -227,7 +228,7 @@ def main(arguments: argparse.Namespace):
             print("\nGoing to the next page")
             next_page_available = True
         # if there is no next page we can end the loop
-        except AttributeError or IndexError:
+        except IndexError:
             next_page_available = False
 
     # assemble message content
@@ -237,15 +238,16 @@ def main(arguments: argparse.Namespace):
         print(betreff)
         print(content)
         # save results for review
-        content_json = json.dumps(content, indent=4)
+        content_json = json.dumps(new_ads_dict, indent=4)
         with open("flat_content.json", "w") as outfile:
             outfile.write(content_json)
 
-    # Todo Loop over several pages
     # Todo count pages and mention page in comment
+    # Todo fix problem with German special characters when saving content to json
     # Todo add information on mandatory questions
     # Todo remove balcony filter and create self-made balcony OR garden-filter
     # Todo add list of no-go-words
+    # Todo add proper logging
 
     # close window after sleep period for debugging
     time.sleep(140)
